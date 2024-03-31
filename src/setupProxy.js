d6693a0e6e9dcf800690cc379f3b6279fe13cb82
nimigeanconstantinion <constantin.nimigean@gmail.com>
2024-03-31 17:40:20 +0300

    added proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
     app.use(
         '/server', // This is the API endpoint that will be proxied
         createProxyMiddleware({
-            target: 'http://localhost:5000/', // This is the target server
+            target: 'http://34.247.255.42:5000/', // This is the target server
             changeOrigin: true,
         })
     );
