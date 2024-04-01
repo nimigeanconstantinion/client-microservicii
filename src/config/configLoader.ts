69cc43224517e343b4f65574c75ff9799d065630
nimigeanconstantinion <constantin.nimigean@gmail.com>
2024-04-01 13:18:21 +0300

    cu modif la edge:5000

--- src/config/configLoader.ts
+++ src/config/configLoader.ts
@@ -5,7 +5,8 @@ export let globalConfig: Config | null = null;
 export const loadConfig = async () => {
     try {
 
-        alert("Din config loader: "+process.env.NODE_ENV);
+        alert("STAI Din loaderConfig: "+process.env.REACT_APP_PATH_TO);
+        alert("STAI Din loaderConfig: "+process.env.NODE_ENV);
 
         let configPath = '/config.production.json';
         console.log("-- --0000 ATENTIE   "+process.env.NODE_ENV);
@@ -16,8 +17,7 @@ export const loadConfig = async () => {
         const response = await fetch(configPath);
 
         globalConfig = await response.json() as Config;
-        alert("din load config ies cu url=:");
-        alert(globalConfig.apiUrl);
+        alert("din load config ies cu url=:"+globalConfig.apiUrl);
     } catch (error) {
         console.error('Failed to load config', error);
     }
