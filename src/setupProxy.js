1d2beba0e4ac271c325bfd8b34d971fd27e70ea8
nimigeanconstantinion <constantin.nimigean@gmail.com>
2024-04-04 14:13:13 +0300

    am scos din proxy /

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
     app.use(
         '/server',
         createProxyMiddleware({
-            target: 'http://edge:5000/',
+            target: 'http://edge:5000',
             changeOrigin: true,
         })
     );
