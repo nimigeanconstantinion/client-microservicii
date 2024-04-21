a38c3a624797540715b27ae644b1b8e8376741b8
nimigeanconstantinion <constantin.nimigean@gmail.com>
2024-04-21 19:30:31 +0300

    am comemntat si loadconfig()

--- src/config/configLoader.ts
+++ src/config/configLoader.ts
@@ -5,19 +5,19 @@ export let globalConfig: Config | null = null;
 export const loadConfig = async () => {
     try {
 
-        // alert("STAI Din loaderConfig: "+process.env.REACT_APP_PATH_TO);
-        // alert("STAI Din loaderConfig: "+process.env.NODE_ENV);
+        // // alert("STAI Din loaderConfig: "+process.env.REACT_APP_PATH_TO);
+        // // alert("STAI Din loaderConfig: "+process.env.NODE_ENV);
+        // //
+        // let configPath = '/config.production.json';
+        // // console.log("-- --0000 ATENTIE   "+process.env.NODE_ENV);
+        // if (process.env.NODE_ENV === 'production') {
+        //     configPath = '/config.production.json';
+        // }
         //
-        let configPath = '/config.production.json';
-        // console.log("-- --0000 ATENTIE   "+process.env.NODE_ENV);
-        if (process.env.NODE_ENV === 'production') {
-            configPath = '/config.production.json';
-        }
-
-        const response = await fetch(configPath);
-
-        globalConfig = await response.json() as Config;
-        // alert("din load config ies cu url=:"+globalConfig.apiUrl);
+        // const response = await fetch(configPath);
+        //
+        // globalConfig = await response.json() as Config;
+        // // alert("din load config ies cu url=:"+globalConfig.apiUrl);
     } catch (error) {
         console.error('Failed to load config', error);
     }
