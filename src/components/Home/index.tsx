4171610071dc93a105ee4e1b38b9ad272dddbd15
nimigeanconstantinion <constantin.nimigean@gmail.com>
2024-04-22 16:09:34 +0300

    cu modificari la spinner.gif

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -30,7 +30,7 @@ import {
     setTotalObjects
 } from "../../store/queryMapStocOptim/queryMapStocOpt.reducer";
 import store from "../../store/store";
-import spinner from "../../Images/spinner.gif"
+import spinner from "../../spinner.gif"
 import WrapperNewHome from "./IndexStyle";
 import Grid from "../GridComp/index"
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
