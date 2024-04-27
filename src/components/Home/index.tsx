0b5842a8e9d03baf2cdb058d5e6d37ca87a33c64
nimigeanconstantinion <constantin.nimigean@gmail.com>
2024-04-27 17:29:46 +0300

    de la src am pus gif

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -31,7 +31,7 @@ import {
 } from "../../store/queryMapStocOptim/queryMapStocOpt.reducer";
 import store from "../../store/store";
 // require('../../Images/spinner.gif');
-import spinner from "../../spinner.gif";
+import spinner from "../../../src/Images/spinner.gif";
 import WrapperNewHome from "./IndexStyle";
 import Grid from "../GridComp/index"
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
@@ -746,8 +746,8 @@ const Homes:React.FC=()=> {
                         {
                             spin>0?(
                                 <>
-                                    {/*<img className={"imgspin"} src={spinner} alt={"My gif"}/>*/}
-                                    <p>Incarc.....</p>
+                                    <img className={"imgspin"} src={spinner} alt={"My gif"}/>
+                                    {/*<p>Incarc.....</p>*/}
                                 </>
                             ):(
                                 shMess?(
