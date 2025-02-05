725b8b14cd778360d963c01000060b8ead93374c
nimigeanconstantinion <constantin.nimigean@gmail.com>
2025-02-05 17:27:08 +0200

    commit 05022025 1727

--- src/Api.tsx
+++ src/Api.tsx
@@ -28,7 +28,7 @@ export default class Api{
             console.log("++ Nu aveam basepath")
             basepath = "http://localhost:5000"; // fallback if config fails
         }
-        basepath="http://react-app.local";
+        // basepath="http://react-app.local";
         const url= basepath+"/server"+ path;
 
         //
