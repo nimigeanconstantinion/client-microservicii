9ae573cf399797eae7154ebeb072bccc05c14a5a
nimigeanconstantinion <constantin.nimigean@gmail.com>
2025-02-06 19:35:14 +0200

    commit 06022025 cu pag de 150 inreg

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -298,7 +298,7 @@ const Homes:React.FC=()=> {
         let k=1;
         if(lista.length>100){
             while(lista.length>0){
-                let pag=lista.splice(0,50);
+                let pag=lista.splice(0,150);
                 console.log(pag);
                 await bulkAdd(pag);
 
